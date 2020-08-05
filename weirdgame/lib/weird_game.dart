8afbc0602576faf02339335a038ec265d1fd84ac
Aryan <aryanjalali1234@gmail.com>
2020-08-06 03:24:55 +0530

    This project deals with the introductions

---
+++ weirdgame/lib/weird_game.dart
@@ -0,0 +1,49 @@
+import 'dart:ui';
+import 'package:flutter/gestures.dart';
+import 'package:flame/game.dart';
+
+class WeirdGame extends Game {
+  Size screenSize;
+  bool hasWon = false;
+
+  void render(Canvas canvas) {
+    // Background
+    Rect bgRect = Rect.fromLTWH(0, 0, screenSize.width, screenSize.height);
+    Paint bgPaint = Paint();
+    bgPaint.color = Color(0xff453874);
+    canvas.drawRect(bgRect, bgPaint);
+
+    // Box in middle
+    double screenCenterX = screenSize.width / 2;
+    double screenCenterY = screenSize.height / 2;
+    Rect boxRect =
+        Rect.fromLTWH(screenCenterX - 75, screenCenterY - 75, 150, 150);
+    Paint boxPaint = Paint();
+    if (hasWon) {
+      boxPaint.color = Color(0xff00ff00);
+    } else {
+      boxPaint.color = Color(0xffffffff);
+    }
+    canvas.drawRect(boxRect, boxPaint);
+  }
+
+  void update(double t) {
+    // TODO: implement update
+  }
+
+  void onTapDown(TapDownDetails d) {
+    double screenCenterX = screenSize.width / 2;
+    double screenCenterY = screenSize.height / 2;
+    if (d.globalPosition.dx >= screenCenterX - 75 &&
+        d.globalPosition.dx <= screenCenterX + 75 &&
+        d.globalPosition.dy >= screenCenterY - 75 &&
+        d.globalPosition.dy <= screenCenterY + 75) {
+      hasWon = true;
+    }
+  }
+
+  void resize(Size size) {
+    screenSize = size;
+    super.resize(size);
+  }
+}
